65c970200241bffb8f090e64d4907f9eb9b7b4cb
kopas <kopasunder@example.com>
2021-05-08 15:35:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var name = "Talgat";
+var age = 17;
+var course= 'ITacademi';
+var town = 'Osh';
+var floor = 'man';
+var yearBorn = '2004';
+var personalBase = 'My name is' + ' ' + name + ', ' + ' and i am' + ' ' + age + ' ' + 'years old!' + ' ' + 'I go to IT courses, its name is' + ' ' + course + '. ' + 'I live in the' + ' ' + town + ' ' + '. ' + "My genger it the" + ' ' + floor + '. ' + 'I was born in' + ' ' + yearBorn;
+console. log(personalBase)
+
+
+
+
+var a = 243;
+var b = 120;
+var result = a / b + a - b ;
+console.log(result)
+
+
+console.log(true + true + true - false + true - true - false + true);
